6f282eb34eb5149af3900083b6192efc30491cb8
Edison Neto <ednetoali@gmail.com>
2019-05-12 17:58:30 -0300

    Update documentation

--- rstn/rstn_service.py
+++ rstn/rstn_service.py
@@ -1,9 +1,10 @@
 from rstnpy import RSTN
-from rstnpy.exceptions import FileNotFoundOnServerError
+from rstnpy.exceptions import (
+    FileNotFoundOnServerError, InvalidDateError,
+    FileNotFoundError
+)
 
 from flask import Blueprint, Response, request, jsonify
-
-from utils import check_date
 
 rstn_page = Blueprint("rstn", __name__)
 
@@ -14,23 +15,26 @@ def index():
     return jsonify(data)
 
 
-@rstn_page.route("/get_data", methods=["GET"])
+@rstn_page.route(
+    "/get_data/",
+    methods=["GET"]
+)
 def rstn_data():
     year = request.args["year"]
     month = request.args["month"]
     day = request.args["day"]
     station = request.args["station"]
 
-    if not check_date(year, month, day):
-        msg = {"message": "Impossible date"}
-        return jsonify(msg), 400
-
     path = "data"
     rstn = RSTN(year, month, day, path, station)
+
     try:
         filename = rstn.downloader.download_file()
     except FileNotFoundOnServerError:
         msg = {"message": "File does not exist"}
+        return jsonify(msg), 400
+    except InvalidDateError:
+        msg = {"message": "Impossible date"}
         return jsonify(msg), 400
 
     filename = rstn.decompress_file(filename)
@@ -40,6 +44,6 @@ def rstn_data():
         # Remove rows with the same index.
         rstn_data = rstn_data[~rstn_data.index.duplicated(keep="first")]
         return Response(rstn_data.to_json(), mimetype="application/json")
-    except:
+    except FileNotFoundError:
         error = {"message": "Internal error"}
         return jsonify(error), 500
